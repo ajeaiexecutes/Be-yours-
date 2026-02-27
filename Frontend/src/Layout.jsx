d01f31f9f95f114890f1350d6e117606295d0c63
ajeaiexecutes <ajay.devxtra@gmail.com>
2026-02-27 17:49:15 +0530

    Update Layout.jsx

--- Frontend/src/Layout.jsx
+++ Frontend/src/Layout.jsx
@@ -9,7 +9,9 @@ const Layout = () => {
   return (
     <div className="min-h-screen flex flex-col">
           <Navbar />
-          <Outlet />
+            <main className="flex-1">
+                <Outlet />
+              </main>
           <Footer/>
         
           
