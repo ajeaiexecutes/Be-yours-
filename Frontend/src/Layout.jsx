5b1891285e9492637d19ecbadc8c94f3e4feeb4e
ajeaiexecutes <ajay.devxtra@gmail.com>
2026-02-27 17:40:29 +0530

    Update Layout.jsx

--- Frontend/src/Layout.jsx
+++ Frontend/src/Layout.jsx
@@ -7,7 +7,7 @@ import Footer from './components/UserInterface/Footer'
 const Layout = () => {
     
   return (
-    <div className="flex flex-row ">
+    <div >
           <Navbar />
           <Outlet />
           <Footer/>
