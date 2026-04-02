a5f5f0fefe425c386c143b3a3dceced996dca4f0
ajay <ajay.devxtra@gmail.com>
2026-04-02 12:23:11 +0530

    chore:ui updation on loader

--- Frontend/src/components/UserInterface/Spinner.jsx
+++ Frontend/src/components/UserInterface/Spinner.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default function Spinner() {
   return (
-    <div className='max-w-screen h-screen flex justify-center items-center py-16'>
+    <div className='max-w-screen h-screen flex flex-col justify-center items-center py-16'>
         <div className='w-10 h-10 border-4 border-gray-200 border-t-black rounded-full animate-spin'></div>
-            <h2 className="text-xl font-semibold text-gray-800">Please Wait...</h2>
+        <h2 className="text-xl font-semibold text-gray-800">Please Wait...</h2>
 
 
     </div>
